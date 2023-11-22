708707e07f3296d6934517211c74f7db8a265570
David Orbang <orbangdavid@yahoo.com>
2023-11-22 20:14:09 +0100

    restarting the project

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import viteSvgr from 'vite-plugin-svgr'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [viteSvgr(), react()],
-})
