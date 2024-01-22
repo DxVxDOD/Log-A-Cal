6518f4196c3a3a6cd18284d07d42022cbec2862d
David Orbang <orbangdavid@yahoo.com>
2024-01-22 20:27:38 +0200

    start

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<Main />
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
